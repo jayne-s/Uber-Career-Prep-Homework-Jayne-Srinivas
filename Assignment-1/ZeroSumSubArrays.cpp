406b6f06397d8d2b1de3c10cab9425930f963c00
Jayne Srinivas <87911188+jayne-s@users.noreply.github.com>
2023-03-15 10:09:37 -0500

    Update ZeroSumSubArrays.cpp

--- Assignment-1/ZeroSumSubArrays.cpp
+++ Assignment-1/ZeroSumSubArrays.cpp
@@ -1,47 +1,35 @@
-// Technique: Variable-size (shrinking/growing) sliding window
+// Technique: Reset/catch-up two-pointer
 // Time Complexity: O(n^2)
 // Space Complexity: O(n)
 
-//(Please note that this solution does not work for all cases such as when the first element itself is 0.)
-
 #include <iostream>
-using namespace std;
 #include <vector>
+using namespace std;
 
-
-int ZeroSumSubArrays(vector<int> nums){
-    int tempSum = 0;
-    int start = 0;
-    int count = 0;
-    for(int i  = 0; i < nums.size(); i++){
-      start = i+1;
-    for(; start < nums.size();){
-        tempSum+=(nums[start]);
-         if(tempSum == 0){
-             count++;
-             tempSum = 0;
-         }
-      else if(nums[start] == 0){
-        count++;
-        tempSum = 0;
+int ZeroSumSubArrays(vector<int> ints) {
+     int count = 0;
+  for (int startPtr = 0; startPtr < ints.size(); startPtr++) {
+          int sum = 0;
+for (int endPtr = startPtr; endPtr < ints.size(); endPtr++) {
+          sum += ints[endPtr];
+          if (sum == 0){
+            count++;
+          }
+            
       }
-         start++;
-    }
-      
-    }
-    return count++;
-}
-
+        
+  }
+  return count;
+} 
 
 int main() {
-
-
-    vector<int> nums1{4, 5, 2, -1, -3, -3, 4, 6, -7};
-    cout << ZeroSumSubArrays(nums1) << endl;
-    //Output: 2
-    //(Subarrays: [5, 2, -1, -3, -3], [-3, 4, 6, -7])
     
-    vector<int> nums2{1, 8, 7, 3, 11, 9};
+  vector<int> nums1 {4, 5, 2, -1, -3, -3, 4, 6, -7}; 
+    cout << ZeroSumSubArrays(nums1) << endl;
+  // Output: 2
+  //(Subarrays: [5, 2, -1, -3, -3], [-3, 4, 6, -7])
+  
+   vector<int> nums2{1, 8, 7, 3, 11, 9};
     cout << ZeroSumSubArrays(nums2) << endl;
     //Output: 0
 
@@ -49,8 +37,9 @@ int main() {
     cout << ZeroSumSubArrays(nums3) << endl;
     //Output: 2
     //(Subarrays: [0], [8, -5, 0, -2, 3, -4])
-  
+
+    
     return 0;
 }
 
-// Time Spent: 1.5 hours
+// Time Spent: 1.5 hour
